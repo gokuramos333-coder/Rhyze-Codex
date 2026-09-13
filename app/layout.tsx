75c0e0085849095e999979540bb189b5d2f6b1ff
GokuRamos <gokuramos@GokuRamoss-Mac-mini.local>
2026-09-13 12:18:26 -0400

    feat: add Meta Pixel base code

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,30 @@ export default function RootLayout({
             gtag('config', 'G-7FTQSRY5FL');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2172162547048825');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element -- Meta Pixel requires a raw noscript tracking pixel. */}
+          <img
+            height="1"
+            width="1"
+            alt=""
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2172162547048825&ev=PageView&noscript=1"
+          />
+        </noscript>
         <a
           href="#content"
           className="sr-only focus:not-sr-only focus:fixed focus:left-4 focus:top-4 focus:z-[80] focus:rounded-full focus:bg-rhyze-coral focus:px-4 focus:py-2 focus:text-sm focus:font-semibold focus:text-rhyze-black"
